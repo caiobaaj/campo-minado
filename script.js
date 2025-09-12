f113d8434cffe958b5b38dc6a0f92c9e58b0e9fd
Caio Teodoro <caioteodoro2012@gmail.com>
2025-09-12 15:55:39 -0300

    adição listaBombas e listaValida

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() { 
     const grid = document.querySelector('.grid')
-
-    console.log(grid);
-
     const width = 10
+    const bandeirasFaltando = document.querySelector('#bandeiras-faltando')
+    let quantiaBombas = 20
+    
 
     function createBoard() {
 
+        bandeirasFaltando.innerHTML = quantiaBombas
+
+        const listaBombas = Array(quantiaBombas).fill('bomba')
+        console.log(listaBombas)
+
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
+            square.id = i
             grid.appendChild(square)
         }
     }

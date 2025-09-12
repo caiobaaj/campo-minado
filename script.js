83cb52969154f20de1d208b6b7013630481f57ec
Caio Teodoro <caioteodoro2012@gmail.com>
2025-09-12 15:30:22 -0300

    função createBoard

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { 
     const grid = document.querySelector('.grid')
 
     console.log(grid);
+
+    const width = 10
+
+    function createBoard() {
+
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div')
+            grid.appendChild(square)
+        }
+    }
+    createBoard()
+
 })
 

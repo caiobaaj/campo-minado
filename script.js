2cf26517af318d8fa4ea7861b692683b79661f17
Caio Teodoro <caioteodoro2012@gmail.com>
2025-09-18 19:53:46 -0300

    etapa onde os alunos pararam na aula do dia 12/09/25

--- script.js
+++ script.js
@@ -3,27 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const tamanhoEixo = 10
     const bandeirasFaltando = document.querySelector('#bandeiras-faltando')
     let quantiaBombas = 20
-    let quadrados = []
 
     function createBoard() {
 
         bandeirasFaltando.innerHTML = quantiaBombas
 
         const listaBombas = Array(quantiaBombas).fill('bomba')
-        console.log(listaBombas)
         const listaValida = Array(tamanhoEixo * tamanhoEixo - quantiaBombas).fill('valido')
-        console.log(listaValida)
         const listaJogo = listaValida.concat(listaBombas)
-        console.log(listaJogo)
         const listaEmbaralhada = listaJogo.sort(() => Math.random() - 0.5);
-        console.log(listaEmbaralhada)
 
         for (let i = 0; i < tamanhoEixo * tamanhoEixo; i++) {
             const square = document.createElement('div')
             square.id = i
             square.classList.add(listaEmbaralhada[i])
             grid.appendChild(square)
-            quadrados.push(square)
         }
     }
     createBoard()

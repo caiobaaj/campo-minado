87ac3810dc3e10389c1fc59598e2253033272883
Caio Teodoro <caioteodoro2012@gmail.com>
2025-09-18 18:36:59 -0300

    estilização quadrados campo minado

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() { 
     const grid = document.querySelector('.grid')
-    const width = 10
+    const tamanhoEixo = 10
     const bandeirasFaltando = document.querySelector('#bandeiras-faltando')
     let quantiaBombas = 20
-    
+    let quadrados = []
 
     function createBoard() {
 
@@ -11,11 +11,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const listaBombas = Array(quantiaBombas).fill('bomba')
         console.log(listaBombas)
+        const listaValida = Array(tamanhoEixo * tamanhoEixo - quantiaBombas).fill('valido')
+        console.log(listaValida)
+        const listaJogo = listaValida.concat(listaBombas)
+        console.log(listaJogo)
+        const listaEmbaralhada = listaJogo.sort(() => Math.random() - 0.5);
+        console.log(listaEmbaralhada)
 
-        for (let i = 0; i < width * width; i++) {
+        for (let i = 0; i < tamanhoEixo * tamanhoEixo; i++) {
             const square = document.createElement('div')
             square.id = i
+            square.classList.add(listaEmbaralhada[i])
             grid.appendChild(square)
+            quadrados.push(square)
         }
     }
     createBoard()

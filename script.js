f43e0dafa1b14c15a0da999f235a702a2ee94492
Caio Teodoro <caioteodoro2012@gmail.com>
2025-09-19 10:44:58 -0300

    estilização dos quadrados

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() { 
-    const grid = document.querySelector('.grid')
-    const tamanhoEixo = 10
+document.addEventListener('DOMContentLoaded', function() {                                      // garante que todo o conteúdo do HTML seja
+    const grid = document.querySelector('.grid')                                                // carregado antes das ações do js
+    const tamanhoEixo = 10 
     const bandeirasFaltando = document.querySelector('#bandeiras-faltando')
     let quantiaBombas = 20
 
-    function createBoard() {
+    function createBoard() {                                                                    // função responsável pela criação do tabuleiro do jogo
 
         bandeirasFaltando.innerHTML = quantiaBombas
 
-        const listaBombas = Array(quantiaBombas).fill('bomba')
+        const listaBombas = Array(quantiaBombas).fill('bomba')  
         const listaValida = Array(tamanhoEixo * tamanhoEixo - quantiaBombas).fill('valido')
-        const listaJogo = listaValida.concat(listaBombas)
-        const listaEmbaralhada = listaJogo.sort(() => Math.random() - 0.5);
+        const listaJogo = listaValida.concat(listaBombas)                                       // lista concatenada
+        const listaEmbaralhada = listaJogo.sort(() => Math.random() - 0.5);                     // algoritmo de aleatorização das bombas no grid
 
-        for (let i = 0; i < tamanhoEixo * tamanhoEixo; i++) {
+        for (let i = 0; i < tamanhoEixo * tamanhoEixo; i++) {                                   // para cada quadrado, é atribuído um id próprio,
+                                                                                                // uma classe válida ou bomba e anexada como filha do grid
             const square = document.createElement('div')
             square.id = i
             square.classList.add(listaEmbaralhada[i])

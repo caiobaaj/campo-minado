f1d41dcc10b6e39bf244e933fc2071813c270f9a
Caio Teodoro <caioteodoro2012@gmail.com>
2025-09-19 11:41:48 -0300

    início funções de clique normal e clique bandeira

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {                                      // garante que todo o conteúdo do HTML seja
-    const grid = document.querySelector('.grid')                                                // carregado antes das ações do js
+document.addEventListener('DOMContentLoaded', function documentoCarregado() {      // garante que todo o conteúdo do HTML seja arregado antes das ações do js
+    const grid = document.querySelector('.grid')         
     const tamanhoEixo = 10 
     const bandeirasFaltando = document.querySelector('#bandeiras-faltando')
     let quantiaBombas = 20
+    let listaQuadrados = []
 
-    function createBoard() {                                                                    // função responsável pela criação do tabuleiro do jogo
+    function createBoard() {        // função responsável pela criação do tabuleiro do jogo
 
         bandeirasFaltando.innerHTML = quantiaBombas
 
         const listaBombas = Array(quantiaBombas).fill('bomba')  
         const listaValida = Array(tamanhoEixo * tamanhoEixo - quantiaBombas).fill('valido')
-        const listaJogo = listaValida.concat(listaBombas)                                       // lista concatenada
-        const listaEmbaralhada = listaJogo.sort(() => Math.random() - 0.5);                     // algoritmo de aleatorização das bombas no grid
+        const listaJogo = listaValida.concat(listaBombas)                           // lista concatenada
+        const listaJogoEmbaralhada = listaJogo.sort(() => Math.random() - 0.5);     // algoritmo de aleatorização das bombas no grid
 
-        for (let i = 0; i < tamanhoEixo * tamanhoEixo; i++) {                                   // para cada quadrado, é atribuído um id próprio,
-                                                                                                // uma classe válida ou bomba e anexada como filha do grid
-            const square = document.createElement('div')
-            square.id = i
-            square.classList.add(listaEmbaralhada[i])
-            grid.appendChild(square)
+        for (let i = 0; i < tamanhoEixo * tamanhoEixo; i++) {       // para cada quadrado, é atribuído um id próprio, uma classe válida ou bomba e anexada como filha do grid
+                                                            
+            const quadrado = document.createElement('div')
+            quadrado.id = i
+            quadrado.classList.add(listaJogoEmbaralhada[i])
+            grid.appendChild(quadrado)
+            listaQuadrados.push(quadrado)       // adiciona os quadrados na lista
+            
+            quadrado.addEventListener('click', function cliqueNormal() {        // função do clique com botão esquerdo, apenas revela o bloco clicado
+
+                click(quadrado)
+            })
+
+            quadrado.addEventListener('click', function cliqueBandeira() {      // função do clique para colocar bandeiras, botao direito
+
+                //addBandeira(quadrado)
+            })
         }
+
     }
+
     createBoard()
 
+    function click(quadrado) {
+        console.log(quadrado)
+    }
 })
 
